5ab1cb2ba7d418a6e6c23273525e9c85be54faa9
Bruno Salmon <dev.salmonb@gmail.com>
2018-11-28 09:14:26 +0000

    Added some comment to the scenario

--- gatling-scenario/src/test/scala/WebsocketSimulation.scala
+++ gatling-scenario/src/test/scala/WebsocketSimulation.scala
@@ -15,10 +15,11 @@ class WebsocketSimulation extends Simulation {
   val scn = scenario("WebsocketSimulation")
     .exec(ws("Connect WS").connect("/eventbus/websocket")
       .onConnected(
-        exec(ws("Login WS")
+        exec(ws("Send WS Text Frame Message")
           .sendText("""{"type":"send", "address":"myAddress", "body":"myBody", "replyAddress":"8d7f27a2-b87d-493f-8ad8-4950463eabc7"}""")
           .await(5 seconds)(
-            ws.checkTextMessage("")
+            ws.checkTextMessage("Receive WS Text Frame Reply") // Not Vert.x compliant as Vert.x sends a binary frame, but no other choice offered by Gatling when using sendText()
+            //ws.checkBinaryMessage("Receive WS Binary Frame Repy") // could be Vert.x compliant but doesn't compile with Gatling
           )
         )
           .pause(1)
